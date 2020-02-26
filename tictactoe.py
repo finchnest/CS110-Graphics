884ab7fc2e600be8ff42ef61251be7ff57ff5bd1
Abeni <abenitamene@gmail.com>
2020-02-26 00:16:00 -0600

    except ground and tree
    Signed-off-by: Abeni <abenitamene@gmail.com>

--- tictactoe.py
+++ tictactoe.py
@@ -1,40 +1,71 @@
+import random
+
 values_dictionary = {1: "", 2: "", 3: "", 4: "", 5: "", 6: "", 7: "", 8: "", 9: ""}
 
-while True:
-    user_choice = input("Do you want to be x or o?")
-    comp_choice = ""
 
-    if (user_choice == "x"):
-        comp_choice = "o"
-    elif (user_choice == "o"):
-        comp_choice = "x"
-    go_first = input("do you wanna go first?")
-    starter = ""
-    if (go_first == "y"):
-        starter = "user"
-    else:
-        starter = "comp"
+def printUpdatedBoard():
+    print(values_dictionary[7] + "      |" + values_dictionary[8] + "       |" + values_dictionary[9])
+    print("-" * 15)
+    print(values_dictionary[4] + "      |" + values_dictionary[5] + "       |" + values_dictionary[6])
+    print("-" * 15)
+    print(values_dictionary[1] + "      |" + values_dictionary[2] + "       |" + values_dictionary[3])
 
-    ctr = 7
-    while ctr > 0:
-        for x in range(ctr, ctr + 3):
-            print(str(x) + "  | ", end=" ")
-        ctr -= 3
-        print("\n")
-        if (ctr != -2):
-            print("-" * 15)
-    break
 
-    move = input("make a move: ")
+def computerMove(mark):
+    empty_vals = []
+    for k in values_dictionary:
+        if (values_dictionary[k] != "x") or (values_dictionary[k] != "o"):
+            empty_vals.append(k)
+    chosen = random.choice(empty_vals)
+    values_dictionary[chosen] = mark
+    print("computer selected ", chosen)
+
+
+def user_move(mark):
     try:
-        if move.isalnum() and (9 > move > 0):
-            if values_dictionary[move] != "":
-                move = input("make another move, the space is occupied: ")
-                printUpdatedBoard()
-            else:
-                values_dictionary[move] = user_choice
-
-
+        move = int(input("Where do you want to go (1-9)? "))
+        if values_dictionary[move] == True:
+            print("make another move, the space is occupied: ")
+            user_move(mark)
+        else:
+            values_dictionary[move] = mark
+            print("You have selected ", move)
 
     except:
-        move = input("make a right move please : ")
+        print("make a right move please ")
+        user_move(mark)
+
+
+user_choice = input("Do you want to be x or o? ")
+comp_choice = ""
+
+if (user_choice == "x"):
+    comp_choice = "o"
+elif (user_choice == "o"):
+    comp_choice = "x"
+go_first = input("Do you " + str(user_choice).capitalize() + " wanna go first? (y/n) ")
+starter = ""
+if (go_first == "y"):
+    starter = "user"
+else:
+    starter = "comp"
+ctr = 7
+while ctr > 0:
+    for x in range(ctr, ctr + 3):
+        print(str(x) + "  | ", end=" ")
+    ctr -= 3
+    print("\n")
+    if (ctr != -2):
+        print("-" * 15)
+while True:
+
+    if (starter == "user"):
+        user_move(user_choice)
+        printUpdatedBoard()
+        computerMove(comp_choice)
+        printUpdatedBoard()
+    else:
+        computerMove(comp_choice)
+        printUpdatedBoard()
+        user_move(user_choice)
+        printUpdatedBoard()
